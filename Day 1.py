1babe08323d54bdf320c35b8d2dae771c4703023
souparnapal <72145624+souparnapal@users.noreply.github.com>
2021-07-08 20:45:56 +0530

    Day 1 Assignment Submitted

---
+++ Day 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+x=np.arange(0,10)
+y=x*x
+plt.plot(x,y,color='r')
+plt.title('Square of values')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
